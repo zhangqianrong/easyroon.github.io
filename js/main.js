dee9ca9873066df71e8a6a821c5ffdf5d2b43b80
zhangqianrong <rabsic_r@163.com>
2016-04-05 10:27:38 +0800

    Site updated: 2016-04-05 10:27:37

--- js/main.js
+++ js/main.js
@@ -166,4 +166,10 @@ require([], function (){
         });
     }
 
+    //相册
+    if($(".instagram").length) {
+        require(['/js/photo.js', '/fancybox/jquery.fancybox.js', '/js/jquery.lazyload.js'], function(obj) {
+            obj.init();
+        });
+    }
 });
